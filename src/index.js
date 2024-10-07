67f7034268038ef0fc3ecc6f7a4e45681056f573
Zzzz-Dz <2829590007@qq.com>
2024-10-07 13:55:14 +0800

    修复导出错误

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals.js';
 import { RouterProvider } from 'react-router-dom';
-import { router } from './Routers/Router.js';
+import { router } from './Router.js';
 import GlobalUserState from './globalContext.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
