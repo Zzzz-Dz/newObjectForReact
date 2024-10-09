7a30a1d6fbd8f67f5f587711768499e511a89586
Zzzz-Dz <2829590007@qq.com>
2024-10-09 17:03:14 +0800

    1

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
-export default {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  }
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: [require('autoprefixer'), require('postcss-nested')]
+}
+
+module.exports = config
